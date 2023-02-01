8c6b47f5a55788dcfcdf21b73c3d92bf14178237
Jesse Maun <jesse@Jesses-Air.lan>
2023-02-01 00:59:56 -0500

    added error handling and routes for auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+
 const app = express(); 
 app.use(json());
 
@@ -11,6 +15,12 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter); 
 app.use(signupRouter);
+
+app.all('*', async (req, res) => { 
+    throw new NotFoundError();
+}); 
+
+app.use(errorHandler);
 
 
 
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, {Request, Response} from 'express'; 
 import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router(); 
 
@@ -9,15 +11,16 @@ router.post('/api/users/signup', [
      .withMessage('Email must be valid'),
     body('password')
      .trim()
-      .isLength({ min: 4, max: 20})
-      .withMessage('Password must be between 4 and 20 characters')
+      .isLength({ min: 5, max: 21})
+      .withMessage('Password must be between 5 and 21 characters')
 ], (req: Request, res: Response) => { 
     const errors = validationResult(req); 
     if (!errors.isEmpty()) {
-        return res.status(400).send(errors.array());
+        throw new RequestValidationError(errors.array());
     }
 const { email, password } = req.body;
 console.log('Creatinga user...');
+throw new DatabaseConnectionError();
 
 res.send({});
 
